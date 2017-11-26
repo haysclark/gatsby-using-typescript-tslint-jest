c723cc4c039445e14809868fb48311009770ddca
Hays Clark <hays.clark@gmail.com>
2017-11-25 16:09:42 -0800

    replacing content with using-typescript example

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    siteName: `Using Typescript Example`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    // Add typescript stack into webpack
+    `gatsby-plugin-typescript`,
+  ],
 }
